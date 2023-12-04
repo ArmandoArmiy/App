c05424eaca689b23c03b8a24d2b2030b6f1cec33
Armando <161101atc@gmail.com>
2023-12-04 15:48:09 -0600

    Changes all

--- App.ino
+++ App.ino
@@ -13,15 +13,16 @@ DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(9600);
-  Serial2.begin(9600); // Inicializar SoftwareSerial
+  //Serial2.begin(9600); // Inicializar SoftwareSerial
 
-  pinMode(19, OUTPUT); // pin de salida de 
+  pinMode(19, OUTPUT); // pin de salida de temperatura
+  pinMode(21, OUTPUT); // pin de salida de temperatura
   
   pinMode(4, INPUT); // pin de lectura fotoresitencia 1
-  pinMode(21, OUTPUT); // pin de salida de fotoresistencia 1
+  pinMode(22, OUTPUT); // pin de salida de fotoresistencia 1
 
   pinMode(27, INPUT); // pin de lectura de proximidad
-  pinMode(18, OUTPUT); // pin de salida de proximidad
+  pinMode(13, OUTPUT); // pin de salida de proximidad
 }
 
 void loop() {
@@ -29,16 +30,16 @@ void loop() {
   get_illumination(); //Funcion para ejecutar la lectura de Iliminacion
   get_proximity(); //Funcion para ejecutar la lectura de proximidad
   Serial.println("-----------------------------------------------------");
-  delay(2000);
+  delay(3000);
   
 }
 
 void get_proximity(){
   int prox = analogRead(27);
   if (prox > 1500) {
-    digitalWrite(18, HIGH);  // Enciende el pin 19 si detecta un objeto
+    digitalWrite(13, HIGH);  // Enciende el pin 13 si detecta un objeto
   } else {
-    digitalWrite(18, LOW);   // Apaga el pin 19 si no detecta un objeto
+    digitalWrite(13, LOW);   // Apaga el pin 13 si no detecta un objeto
   }
   Serial.println("| Valor del sensor de proximidad: " + String(prox));
 
@@ -47,9 +48,9 @@ void get_proximity(){
 void get_illumination(){
   int illu = analogRead(4);
   if (illu < 1900) {
-    digitalWrite(21, HIGH);  // Enciende el pin 16 si la luz es baja
+    digitalWrite(22, HIGH);  // Enciende el pin 22 si la luz es baja
   } else {
-    digitalWrite(21, LOW);   // Apaga el pin 16 si la luz es alta
+    digitalWrite(22, LOW);   // Apaga el pin 22 si la luz es alta
   }
   Serial.println("| Valor de la iluminación 1: " + String(illu) + "\n");
 }
@@ -60,8 +61,10 @@ void get_Temperature() {
   temperaturaC = sensors.getTempCByIndex(0);
   if (temperaturaC > 20) {
     digitalWrite(19, HIGH);  // Enciende el pin 19 si la temperatura es alta
+    digitalWrite(21, LOW);  
   } else {
-    digitalWrite(19, LOW);   // Apaga el pin 19 si la temperatura es baja
+    digitalWrite(19, LOW);   
+    digitalWrite(21, HIGH); // Enciende el pin 19 si la temperatura es baja
   }
   Serial.println("| Temp. Celsius: " + String(temperaturaC) + "ºC \n");
 }
